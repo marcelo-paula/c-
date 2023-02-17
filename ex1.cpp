1de3fe14352dd149348b8b3473faa97a601c4404
marcelo <marcelo_xavierpaula@hotmail.com>
2023-02-17 17:07:32 -0300

    first commit

---
+++ ex1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	int x = 10;
+	
+	for (int i=0; i< x; i++){
+		cout << i << endl;
+	}
+}
---
+++ exercicio.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+main(){	
+	setlocale(LC_ALL, "Portuguese");
+	
+	float p1,p2,result;
+	
+	cout << "***********************"<<endl;
+	cout << "*******Bem-Vindo*******"<<endl;
+	cout << "***********************"<<endl;
+	
+	cout << "Informe a primeira nota: ";
+	cin >> p1;
+	
+	cout << "Informe a segunda nota: ";
+	cin >> p2;
+	
+	result = (p1+p2) / 2;
+	
+	if (result >= 7){
+		cout << "Você passou com a nota: " << result << " parabéns";
+	}else {
+		cout << "Não foi dessa vez";
+	}
+	
+}
